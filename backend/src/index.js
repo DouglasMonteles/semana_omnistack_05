990a9da414a31560b9da55ced9063ed02d872c58
DouglasMonteles <silvadouglas328@gmail.com>
2020-02-09 17:55:54 -0200

    conexão com banco de dados mongoDB estabelecida

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://goweek:goweek@cluster0-5eznv.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World');
