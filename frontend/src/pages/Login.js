4be590b8ffdd4ca17db6605671bad310fb8e5d7a
DouglasMonteles <silvadouglas328@gmail.com>
2020-02-09 23:02:45 -0200

    Tela de login estilizada e funcionando

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
 
+import './Login.css';
+
+import logoTwitter from './img/twitter.png';
+
 export default class Login extends Component {
+    state = {
+        username: '',
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        
+        const { username } = this.state;
+
+        if (!username) return;
+
+        localStorage.setItem('@GoTwitter:username', username);
+
+        this.props.history.push('/timeline');
+    }
+
+    handleInputChange = (event) => {
+        this.setState({ username: event.target.value });
+    }
+
     render() {
         return (
-            <h1>Login</h1>
+            <div className="login-wrapper">
+                <img src={logoTwitter} alt="twitter"/>
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                        placeholder="Nome de usuário" 
+                    />
+                    <button type="submit">Entrar</button>
+                </form>
+            </div>
         );
     }
 }
