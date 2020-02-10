d5eec2ea6151efa704dd49717ce8bc48379bc8fa
DouglasMonteles <silvadouglas328@gmail.com>
2020-02-09 23:14:19 -0200

    timeline já capitura o valor digitado

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
+import './Timeline.css';
+
+import logoTwitter from './img/twitter.png';
+
 export default class Timeline extends Component {
+    state = {
+        newTweet: ''
+    };
+
+    handleInputChange = (e) => {
+        this.setState({ newTweet: e.target.value });
+    }
+    
     render() {
         return (
-            <h1>Timeline</h1>
+            <div className="timeline-wrapper">
+                <img height={40} src={logoTwitter} alt="twitter"/>
+
+                <form>
+                    <textarea 
+                        value={this.state.newTweet}
+                        onChange={this.handleInputChange}
+                    ></textarea>
+                </form>
+            </div>
         );
     }
 }
