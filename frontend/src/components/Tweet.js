b458019bab98b21ce6a1414f1da0b7906d43e997
DouglasMonteles <silvadouglas328@gmail.com>
2020-02-10 17:10:06 -0200

    componente de renderização de tweets

---
+++ frontend/src/components/Tweet.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './Tweet.css';
+
+export default class Tweet extends Component {
+    render() {
+        return (
+            <h1></h1>
+        );
+    }
+}
